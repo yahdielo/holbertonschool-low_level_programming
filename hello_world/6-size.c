8ea837c57ea89c627449e82dfd4da6aba95a68a4
yahdielo <yahdielaz@gmail.com>
2022-10-01 01:52:47 +0000

    print sizes

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -10,11 +10,11 @@ int main(void)
 	long int li;
 	long long int lli;
 	float f; 
-
 	printf("The size of a char is: %lu.\n", (unsigned long)sizeof(c));
 	printf("The size of an int is: %lu.\n", (unsigned long)sizeof(i));
 	printf("The size of a long int is: %lu.\n", (unsigned long)sizeof(li));
 	printf("The size of a long long int is: %lu.\n", (unsigned long)sizeof(lli));
 	printf("The size of a float is: %lu.\n", (unsigned long)sizeof(f));
+	
 	return(0);
 }
