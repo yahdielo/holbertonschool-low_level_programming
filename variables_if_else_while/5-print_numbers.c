9540dca44601c384533769e7cc59a76d8143200e
yahdielo <yahdielaz@gmail.com>
2022-10-04 19:21:45 +0000

    print numbers from 0 to 10

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - print numbers from 0 to 10
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a = 0;
+
+	while (a <= '9')
+	{
+	putchar(a);
+	a++;
+	}
+	putchar('\n');
+
+	return (0);
+}
