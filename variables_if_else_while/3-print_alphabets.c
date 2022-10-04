a6a5dc67abfc269f1b7324d444b92a4c38dcf9c1
yahdielo <yahdielaz@gmail.com>
2022-10-04 18:20:06 +0000

    print alphabet lower case and upper case

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - print alphabet
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch = 'a';
+	char ch1 = 'A';
+
+	while (ch <= 'z')
+	{
+	putchar(ch);
+	ch++;
+	}
+
+	while (ch1  <= 'Z')
+	{
+	putchar(ch1);
+	ch1++;
+	}
+	putchar('\n');
+
+	return (0);
+}
