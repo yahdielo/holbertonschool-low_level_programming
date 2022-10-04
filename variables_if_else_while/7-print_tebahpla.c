5a7db645310c34e732225414fea7bac69dbf6546
yahdielo <yahdielaz@gmail.com>
2022-10-04 20:34:09 +0000

    print alphabet in reverse

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/*
+ * main - Print alphabet in reverse
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/*declare var */
+	char al;
+
+	/*print from z o a*/
+	for (al = 'z'; al >= 'a'; al--)
+	{
+	putchar(al);
+	}
+	putchar('\n');
+
+	return (0);
+}
