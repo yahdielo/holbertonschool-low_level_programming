b01eee46c8da6d1a39ce5467d7e6193b5ebf97f9
yahdielo <yahdielaz@gmail.com>
2022-10-04 17:42:02 +0000

    print alphabet lower case

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - print alphabet
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/* declare variable */
+	char ch = 'a' ;
+
+	while(ch <= 'z')
+	{
+	putchar(ch);
+	ch++;
+	}
+
+	return(0);
+}
