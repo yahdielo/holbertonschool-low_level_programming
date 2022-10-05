d8f7b26b79af0d59c3e5dcf3ed59d08d83f0e6fe
yahdielo <yahdielaz@gmail.com>
2022-10-05 17:00:22 +0000

    _putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -5,6 +5,14 @@
  */
 int main(void)
 {
-	putchar("_putchar\n")
-		return (0);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+
+	return (0);
 }
