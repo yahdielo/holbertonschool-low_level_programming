02acbc19031f7b275291fd45f54fac0382114f3a
yahdielo <yahdielaz@gmail.com>
2022-10-05 21:11:25 +0000

    return absolute value

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *_abs - cheks and compute the absolute value of a num
+ *@x: computes the absolute value of a number
+ * Return: Always 0 (Success)
+ */
+int _abs(int x)
+{
+	if (x < 0)
+	{
+		x = (-1) * x;
+	}
+	_putchar(x);
+	return (0);
+}
