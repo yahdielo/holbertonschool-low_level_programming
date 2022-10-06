194b8f0c323b8ed9279caff037b14d63f85935d5
yahdielo <yahdielaz@gmail.com>
2022-10-06 02:19:34 +0000

    print haours of the day

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *jack_bauer - print every minute of the day
+ *
+ * Return: Always 0 (Success)
+ */
+void jack_bauer(void)
+{
+	int hrs, min;
+
+	for (hrs = 0; hrs < 23; hrs++)
+	{
+		for (min = 0; min < 60; min++)
+		{
+		_putchar((hrs / 10) + '0');
+		_putchar((hrs % 10) + '0');
+		_putchar(':');
+		_putchar((min / 10) + '0');
+		_putchar((min % 10) + '0');
+		_putchar('\n');
+		}
+	}
+}
