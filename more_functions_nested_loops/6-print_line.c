97d2df47d6ec40b4362fdc53632de97fc63d354e
yahdielo <yahdielaz@gmail.com>
2022-10-07 23:42:27 +0000

    print a line in terminal

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+/**
+ * print_line - print a line in terminal
+ *@n: is the numer od times _ will be printed
+ * Return: Always 0 (Success)
+ */
+void print_line(int n)
+{
+	int i = 0;
+
+	if (n > 0)
+	{
+		while (i <= n)
+		{
+			putchar('_');
+			i++;
+		}
+	 putchar('\n');
+	}
+	else
+	putchar('\n');
+}
