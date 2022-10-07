b658040b1acac68ade643996a2e197ed2cfd9837
yahdielo <yahdielaz@gmail.com>
2022-10-07 21:30:37 +0000

    spirnt number from 0 to 9 skipping 2 and 4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_most_numbers - print numbers 0 to 9 avoid 2 & 4
+ * Return: Always 0 (Success)
+ */
+void print_most_numbers(void)
+{
+	int num;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+		if (num == '2' || num == '4')
+		{
+			num++;
+		}
+	putchar(num);
+	}
+
+	putchar('\n');
+}
