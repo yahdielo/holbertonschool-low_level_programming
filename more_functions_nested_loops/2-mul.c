f62c07c32e033f5546e301e2b117632304f24bf3
yahdielo <yahdielaz@gmail.com>
2022-10-07 18:11:34 +0000

    Multiply 2 ints

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,10 @@
+/**
+ * mul - multiplys 2 integers aand b
+ *@a: functions multiplys 2 ints
+ *@b: secodn varibale to be multiply by a
+ * Return: Always 0 (Success)
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
