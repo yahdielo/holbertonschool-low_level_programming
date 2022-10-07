98a2d5f963a6fe20da10212cb0255b0f89610dc8
yahdielo <yahdielaz@gmail.com>
2022-10-07 17:25:06 +0000

    c in upper return 1 else return 0

---
+++ more_function_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * _isupper - check for c to be a upper case char
+ *@c: checks if c is upper
+ * Return: Always 0 (Success)
+ */
+int _isupper(int c)
+{
+	for ( c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (0);
+	}
+}
