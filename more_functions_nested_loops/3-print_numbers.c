c9be3bcd7a6a3dac3cc2f7913808e253d45e4c29
yahdielo <yahdielaz@gmail.com>
2022-10-07 18:27:42 +0000

    count from 0 to 9

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-#include <main.h>
+#include "main.h"
 /**
  * mul - multiplys 2 integers aand b
  *@a: functions multiplys 2 ints
  *@b: secodn varibale to be multiply by a
  * Return: Always 0 (Success)
  */
-void print_numbers(void);
+void print_numbers(void)
 {
 	for (num = '0'; num =< '9', num++)
 	{
