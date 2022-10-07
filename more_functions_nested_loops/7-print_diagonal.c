f2dec06883d7c7358b25f94bc79ced0a5ec6df24
yahdielo <yahdielaz@gmail.com>
2022-10-07 23:53:15 +0000

    creat a diagonal line

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,22 @@
+#include <stdio>
+#include "main.h"
+/**
+ * print_diagonal - print a diagonal lines
+ *@n: functions multiplys 2 ints
+ * Return: Always 0 (Success)
+ */
+void print_diagonal(void)
+{
+	int i, j;
+
+	if(n < 0)
+	{
+		purchar('/n')
+	}
+
+	for (i = 0; j < i; i++)
+	{
+		putchar(' ');
+	}
+	putchar('\');
+}
