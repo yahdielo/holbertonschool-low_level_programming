d35e87230660c3e0bb2e29c257ea3d9ba786c4e4
yahdielo <yahdielaz@gmail.com>
2022-10-08 03:00:08 +0000

    print lines

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -9,12 +9,11 @@ void print_line(int n)
 {
 	int i;
 
-	if (n < 0)
+	if (n == 0 || n < 0)
 	{
 		putchar('\n');
 	}
-	else
-		for (i = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 		{
 			putchar('_');
 		}
