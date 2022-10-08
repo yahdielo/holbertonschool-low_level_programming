7fb5d7472f788deb3f17fbd5ffae83e3cb7414ae
yahdielo <yahdielaz@gmail.com>
2022-10-08 03:34:00 +0000

    print squares

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -1,8 +1,8 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * main - check the code
- *
+ * print_square - print a square
+ *@size: var that indicates the size of the square
  * Return: Always 0.
  */
 void print_square(int size)
