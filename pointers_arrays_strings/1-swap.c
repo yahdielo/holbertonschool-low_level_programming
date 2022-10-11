261dcb297dcc7acef4cbf5682f2feb03b10c4bbd
yahdielo <yahdielaz@gmail.com>
2022-10-11 03:05:33 +0000

    swap variable values

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -3,10 +3,15 @@
  * swap_int - swap values of variables 
  * @a: value of a swap to b
  * @b: value of b swap to a
- *
  * Return: The result of the addition.
  */
 void swap_int(int *a, int *b)
 {
-	swap(*a,*b);
+		int temp;
+
+		temp = *a;
+
+		*a = *b;
+
+		*b = temp;
 }
