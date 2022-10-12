ce0183486ce369629bc15689d2db3f5e1b9e515a
yahdielo <yahdielaz@gmail.com>
2022-10-12 22:21:14 +0000

    print array elemets

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_array - find lenght of a string and reverse.
+ * @a: pointer of array.
+ * @n: number of lenght of the array.
+ * Return: every element of the array.
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n ; i++)
+	{
+		printf("%d", a[i]);
+		if ( i < (n - 1))
+			printf(", ");
+	}
+	putchar('\n');
+}
