c6755885b58bf941bc249d1d675e4f1da40aeb04
yahdielo <yahdielaz@gmail.com>
2022-10-12 00:17:02 +0000

    print string

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * _puts - function that prints a string
+ * @str: pointer to a sting that should be printed
+ * Return: The string save in pointer str
+ */
+void _puts(char *str)
+{
+	_puts(str);
+}
