8448fbc851a33cd5a33aef57ca03ca018817bb4e
yahdielo <yahdielaz@gmail.com>
2022-10-12 20:59:43 +0000

    print half a string

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -7,7 +7,7 @@ void puts2(char *str)
 {
 	int i;
 
-	for (i = 0; str[i]  != '\0' ; i++)
+	for (i = 0; str[i]  != '\0' ; i += 2)
 	{
 		if (i % 2 == 0)
 		{
---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * swap_int - swap values of variables.
+ * @a: value of a swap to b.
+ * @b: value of b swap to a.
+ * Return:
+ */
+void puts_half(char *str)
+{
+	int l, mid, n;
+
+	/*fint lenght of str */
+	for (l = 0; str[l] != '\0';)
+	{
+		l++;
+	}
+
+	if (l % 2 == 0)
+	{
+		for (mid = l / 2; str[mid] != '\0'; mid++)
+		{
+			_putchar(str[mid]);
+		}
+	}
+	else
+	{
+		for (n = (l - 1) / 2; str[n] != '\0'; n++)
+		{
+			_putchar(str[n]);
+		}
+	}
+	_putchar('\n');
+}
