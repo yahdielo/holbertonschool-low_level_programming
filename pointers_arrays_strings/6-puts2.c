0f7816bac42ec3c977f4305f26778becc1a233ae
yahdielo <yahdielaz@gmail.com>
2022-10-12 18:54:49 +0000

    print every other char of string

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -7,13 +7,12 @@ void puts2(char *str)
 {
 	int i;
 
-	for (i = 0; str[i]  != '\0' ;)
+	for (i = 0; str[i]  != '\0' ; i++)
 	{
-		if (str[i] % 2 == 0)
+		if (i % 2 == 0)
 		{
 			_putchar(str[i]);
 		}
-		i += 2;
 	}
 	_putchar('\n');
 }
