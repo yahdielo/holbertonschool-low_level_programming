2a230c79bc6eb9a3036092f85c5ebd9647dc2c65
yahdielo <yahdielaz@gmail.com>
2022-10-13 00:59:11 +0000

    concatnate 2 strings avoiding null val

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * *_strcat - concatnate 2 arrays together
+ * @dest: first array
+ * @src: second array.
+ * Return: addition of the values of 2 arrays.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+	int j;
+
+	for (i = 0; dest[i] != '\0';)
+	{
+		i++;
+	}
+
+	for (j = 0; j < n && src[j] != '\0';j++)
+	{
+		dest[i + j] = src[j];
+	}
+	dest[i + j] = '\0';
+	return (dest);
+}
