884a78b5741dc40d8a7085da74a4956c6dbee29f
yahdielo <yahdielaz@gmail.com>
2022-10-13 02:49:40 +0000

    reverse content of array

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -1,15 +1,16 @@
 #include "main.h"
 /**
- * _strlen - find lenght of a string.
- * @s: string to find the lenght.
- * Return: string in reverse
+ * reverse_array - reverse the elements of the array
+ * @a: name of the array
+ * @n: numbers of characters in th array
+ * Return: reverse values of the array
  */
 void reverse_array(int *a, int n)
 {
 	int i;
 	int temp;
 
-	for (i = 0; i < n /2;)
+	for (i = 0; i < n / 2;)
 	{
 		temp = a[i];
 		a[i] = a[n - i - 1];
