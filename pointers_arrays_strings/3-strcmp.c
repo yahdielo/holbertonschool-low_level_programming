1eecdb4dd94594baaff906600a26d117e7181ec9
yahdielo <yahdielaz@gmail.com>
2022-10-13 01:57:39 +0000

    compare 2 strings

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strcpy - copies the string pointed
+ * @dest: destination
+ * @src: source
+ * Return: destination
+ */
+int _strcmp(char *s1, char *s2)
+{
+
+	if (*s1 == *s2)
+	{
+		 if (*s1 == '\0')
+		{
+			return (0);
+		}
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
+}
