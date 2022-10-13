c4063ca1a39eb2df020db71d16c9b42df758c1b8
yahdielo <yahdielaz@gmail.com>
2022-10-13 02:00:47 +0000

    compare 2 strings

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -10,12 +10,13 @@ int _strcmp(char *s1, char *s2)
 
 	if (*s1 == *s2)
 	{
-		 if (*s1 == '\0')
+		/** if (*s1 == '\0')
 		{
 			return (0);
 		}
 		s1++;
-		s2++;
+		s2++; */
+		return (0);
 	}
 	return (*s1 - *s2);
 }
