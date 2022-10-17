b2bfcb87cc9c5a594ae6e2d5d2a2ed9d3f5a3ff3
yahdielo <yahdielaz@gmail.com>
2022-10-17 16:47:07 +0000

    get fractional of a number

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -1,14 +1,18 @@
 #include "main.h"
 /**
  * factorial - print factorial numbers
- * @s:  var to rpint the factorial numbers
+ * @n:  var to rpint the factorial numbers
  * Return: print factorial numbers
  */
 int factorial(int n)
 {
-	if (n <= 1)
+	if (n == 1)
 	{
 		return (1);
 	}
-	return n * factorial(n - 1);
+	if (n < 0)
+	{
+		return (-1);
+	}
+	return (n * factorial(n - 1));
 }
