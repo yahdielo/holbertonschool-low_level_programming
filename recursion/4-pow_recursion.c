85aba9acf3280f9de3951dc70021a234b81504c5
yahdielo <yahdielaz@gmail.com>
2022-10-17 17:27:56 +0000

    return the power of x reise to ywq

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * factorial - returns the factorial of a given number
+ * @n: given number
+ * Return: factorial given number
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y == 0)
+	{
+		return (1);
+	}
+	else if (y % 2 == 0)
+	{
+		return _pow_recursion(x, y/ 2) * _pow_recursion(x, y / 2);
+	}
+	else 
+	{
+		return x * _pow_recursion(x, y / 2) * _pow_recursion(x, y / 2);
+	}
+	return (0);
+}
