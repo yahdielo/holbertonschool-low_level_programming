5b4a8cb65213d07eb32b61bec690fceacb7fe29b
yahdielo <yahdielaz@gmail.com>
2022-10-17 15:01:21 +0000

    preint string with recursion loop

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,16 +1,15 @@
 #include "main.h"
 /**
- *
- * _puts_recursion - function to print a string
- * @s: pointer to string
- * Return: String
+ * _puts_recursion - print string using recursion
+ * @s: pointer to first char of string
+ * Return: print string
  */
 void _puts_recursion(char *s)
 {
 	if (*s != '\0')
 	{
 		_putchar (*s);
-		_puts_recursion (s + 1);
+		_puts_recursion(s + 1);
 	}
 	else
 	{
