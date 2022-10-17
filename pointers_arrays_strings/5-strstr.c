7ce3033b51451b4ebec1247c4f69f9fc99528d5c
yahdielo <yahdielaz@gmail.com>
2022-10-17 03:26:07 +0000

    that locates substring and prints it

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -20,10 +20,7 @@ char *_strstr(char *haystack, char *needle)
 		{
 			for (j = 0; needle[j] != '\0'; j++)
 			{
-				if (haystack[i] == needle[0])
-				{
-					return (haystack + i);
-				}
+				return (haystack + i);
 			}
 		}
 	}
