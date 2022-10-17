5adb3e6eb7e8d564d1bac7cb9526e11414aa0960
yahdielo <yahdielaz@gmail.com>
2022-10-17 17:34:22 +0000

    return the power of x reise to y

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -17,7 +17,7 @@ int _pow_recursion(int x, int y)
 	}
 	else if (y % 2 == 0)
 	{
-		return _pow_recursion(x, y / 2) * _pow_recursion(x, y / 2);
+		return (_pow_recursion(x, y / 2) * _pow_recursion(x, y / 2));
 	}
 	else
 	{
