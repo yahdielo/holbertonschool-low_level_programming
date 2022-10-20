3e14ec6027d25594f19b8651090146e4312d1872
yahdielo <yahdielaz@gmail.com>
2022-10-20 17:14:38 +0000

    print name of the program

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,5 +1,9 @@
 #include "stdio.h"
-
+/**
+ * main - get the name of the program
+ * @argv: argument vector, get the amount of arguement.
+ * Return: return name of the program
+ */
 int main(int __attribute__ ((unused)) argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
