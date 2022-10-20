87f0b3c7c346259e068476da168cd8032aecc20e
yahdielo <yahdielaz@gmail.com>
2022-10-20 17:47:32 +0000

    print number of arguments feed to the program

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -5,15 +5,8 @@
  * @argv: argument vector
  * Return: number of arguments  to the program
  */
-int main(int argc, char *argv[])
+int main(int argc, char __attribute__ ((unused)) *argv[])
 {
-	int i;
-
-	for (i = 0; i < argc; i++)
-	{
-		/*prnt all arguments*/
-		printf("%s", argv[i]);
-	}
-	printf("\n");
+	printf("%d", argc);
 	return (0);
 }
