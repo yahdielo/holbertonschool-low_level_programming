f1625ee2795354fd447f8748c6d687f7831dc834
yahdielo <yahdielaz@gmail.com>
2022-10-20 17:27:51 +0000

    print number of arguments feed to the program

---
+++ argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - fucntion that returns number of arguments
+ * @argc: argument constant
+ * @argv: argument vector
+ * Return: number of arguments  to the program
+ */
+int main(int arc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i+=)
+	{
+		/*prnt all arguments*/
+		printf("%s", argv[i]);
+	}
+	printf("\n");
+}
