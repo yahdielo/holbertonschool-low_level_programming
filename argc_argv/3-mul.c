5a86fca650d88bfad48e07b320e4fdb8ce078bf0
yahdielo <yahdielaz@gmail.com>
2022-10-20 19:16:14 +0000

    multiply aguments

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - get the name of the program
+ * @argv: argument vector, get the amount of arguement.
+ * @argc: argument constant
+ * Return: return sum of two intergers
+ */
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	int a, b, mul;
+
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+
+	mul = a * b;
+
+	printf("%d", mul);
+
+	return (0);
+}
