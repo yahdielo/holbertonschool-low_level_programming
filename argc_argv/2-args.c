3b2100db9b3c3a706b9efe457ad810dddb55591f
yahdielo <yahdielaz@gmail.com>
2022-10-20 18:36:24 +0000

    print all arguments

---
+++ argc_argv/2-args.c
@@ -0,0 +1,12 @@
+#include "stdio.h"
+/**
+ * main - get the name of the program
+ * @argv: argument vector, get the amount of arguement.
+ * @argc: un use parameter
+ * Return: return name of the program
+ */
+int main(int __attribute__ ((unused)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
