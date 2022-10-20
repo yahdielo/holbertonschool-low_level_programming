742d272afaf5576ce39dfacfdf1ec0580f5ded14
yahdielo <yahdielaz@gmail.com>
2022-10-20 20:15:33 +0000

    multiply aguments

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -12,7 +12,7 @@ int main(int __attribute__((unused)) argc, char *argv[])
 
 	      if (argc != 3)
         {
-                printf("Error");
+                printf("Error\n");
                 return (1);
         }
 
