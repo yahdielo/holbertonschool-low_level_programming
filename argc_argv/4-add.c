be19442b88c2af3dbea80b60b42bb0c612e0fc4d
yahdielo <yahdielaz@gmail.com>
2022-10-21 22:42:22 +0000

    sum calculator

---
+++ argc_argv/4-add.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - name of the program
+ * @argv: argument vector, get the amount of arguement.
+ * @argc: argument constant
+ * Return: return sum of two intergers
+ */
+int main(int argc, char *argv[])
+{
+	int i, add;
+
+	for (i = 0; i < argc; i++)
+	{
+		add += atoi(argv[i]);
+		printf ("%d\n", add);
+	}
+	return (0);
+}
