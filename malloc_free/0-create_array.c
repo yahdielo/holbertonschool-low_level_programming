b2072bb90b15522678608d9a0bdf582e4a9f56d8
yahdielo <yahdielaz@gmail.com>
2022-10-24 18:57:09 +0000

    function that creats an array

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * create_array - creat array with value allocation of specific data type
+ *
+ *
+ */
+char *create_array(unsigned int size, char c)
+{
+	/*creat a array of char wth a specific char*/
+	/* creat a pointer to array and separate memory of a specific data type*/
+	char *p = malloc(sizeof(char) * size);
+	unsigned int i;	
+	/* if size is 0 return null*/
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	/*run inside the array with i & subtitud with value c*/
+	for (i = 0; i < size; i++)
+	{
+		p[i] = c;
+	}
+	/*returns the address of the first value in pointer p*/
+	return (p);
+}
---
+++ malloc_free/main.h
@@ -0,0 +1,2 @@
+int _putchar(int c);
+char *create_array(unsigned int size, char c);
