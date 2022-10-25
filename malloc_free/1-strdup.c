767da6031ba3d8c09d3ac9f447ad89dd5ce3808a
yahdielo <yahdielaz@gmail.com>
2022-10-25 17:19:39 +0000

    return a pointer to the duplcate of string str

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -11,7 +11,7 @@
 char *_strdup(char *str)
 {
 	unsigned int i, j, size = strlen(str);
-	char *s = malloc((i + 1)sizeof(char) * size);
+	char *s = malloc(sizeof(char) * size + 1);
 
 	/* if str is null return null*/
 	if (str == NULL)
