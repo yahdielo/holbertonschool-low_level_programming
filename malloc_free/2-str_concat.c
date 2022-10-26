2bc91aad2c38e4035af75cfb798e0884e46cad3d
yahdielo <yahdielaz@gmail.com>
2022-10-26 02:44:40 +0000

    return copy to the pointer

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -15,14 +15,6 @@ char *str_concat(char *s1, char *s2)
 	char *concat = malloc(sizeof(char) * maxsize + 1);
 
 	/* if one of the string is null return null*/
-	if (s1 == NULL)
-	{
-		return (s2);
-	}
-	if (s2 == NULL)
-	{
-		return (s1);
-	}
 
 	if (concat == NULL)
 	{
