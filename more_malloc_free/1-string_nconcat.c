0fb0cdc5843fb6ae938a95b30d55c14fcb5cd802
yahdielo <yahdielaz@gmail.com>
2022-10-28 16:36:40 +0000

    return pointer to stroing, concat with malloc

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,60 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+/**
+ * string_nconcat - function to concatnate string with malloc
+ * @s1: first string
+ * @s2: second string
+ * @n: variable like size
+ * Return: pointer to string
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i, len1, len2, maxsize;
+	char *ptr;
+
+	if (s1 == NULL)
+	{
+		len1 = 0;
+	}
+	else
+	{
+		len1 = strlen(s1);
+	}
+
+	if (s2 == NULL)
+	{
+		len2 = 0;
+	}
+	else
+	{
+		len2 = strlen(s2);
+	}
+
+	maxsize = len1 + len2;
+	ptr = malloc(sizeof(char) * (maxsize + n + 1));
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+
+	if (n >= len2)	
+	{
+		n = len2;	
+	}
+
+	for (i = 0; i < len1; i++)
+	{
+		ptr[i] = s1[i];
+	}
+
+	for (i = 0; i < n; i++)
+	{
+		ptr[len1 + i] = s2[i];
+	}
+
+	ptr[len1] = '\0';
+	return (ptr);
+}
