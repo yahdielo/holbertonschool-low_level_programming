c17d143f6bf44211a4aa4a313c8ce9d94631456d
yahdielo <yahdielaz@gmail.com>
2022-10-31 17:10:35 +0000

    initialize variable type struct

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -3,10 +3,9 @@
  *
  *
  */
-void init_dog(struct *d, char *name, float age, char *owner)
+void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	struct *ptr;
-
+	struct dog;
 	d->name = name;
 	d->owner = owner;
 	d->age = age;
