c9a49a200c7418920d01742796179b021f221e12
yahdielo <yahdielaz@gmail.com>
2022-10-31 16:14:15 +0000

    creat a struc called dog

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,8 +1,10 @@
-#include <stdlib.h>
+#ifndef DOG_H
+#define DOG_H
 /**
- *
- * dog - struct dog, contains var of dog description
- *
+ * struct dog - struct dog, contains var of dog description
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: dog owner
  */
 struct dog
 {
@@ -10,3 +12,4 @@ struct dog
 	float age;
 	char *owner;
 };
+#endif
