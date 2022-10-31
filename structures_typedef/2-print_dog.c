9b7aea62b3f8917b64e1061c95731e8dc3a79362
yahdielo <yahdielaz@gmail.com>
2022-10-31 19:36:02 +0000

    print struct dog

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -17,12 +17,12 @@ void print_dog(struct dog *d)
 	}
 	if (d->name == NULL)
 	{
-		printf("Name: nil\n");
+		printf("Name: (nil)\n");
 		printf("Age: %f\n", d->age);
 	}
 	if (d->owner == NULL)
 	{
-		printf("Owner: nil\n");
+		printf("Owner: (nil)\n");
 	}
 	else
 	{
