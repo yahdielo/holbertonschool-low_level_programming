c61770dc9f50bb5ae6b19e1e19417cbe8137ec9d
yahdielo <yahdielaz@gmail.com>
2022-11-01 17:05:56 +0000

    creat new dog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -7,8 +7,9 @@
  *@name: name of dog
  *@age: age of dog
  *@owner: owner of dog
+ *Return: newdog pointer
  */
-dog_t *new_dog(char *name,float age, char *owner)
+dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *newdog;
 
