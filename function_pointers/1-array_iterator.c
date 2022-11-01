4c79f6e262f2dcdaddfdac5ef41ec5944f62a001
yahdielo <yahdielaz@gmail.com>
2022-11-01 20:49:25 +0000

    array iteration

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -5,7 +5,7 @@
  * array_iterator - function to call function pointer and iterate on array
  * @size: size of the array
  * @action: functions to use for printing
- *
+ * @array: string to print
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
@@ -16,12 +16,12 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 		void (*p) (int);
 		p = action;
 
+		if (array == NULL)
+		{}
+
 		for (i = 0; i < size; i++)
 		{
 			(*p)(array[i]);
 		}
-	}
-	if (array == NULL)
-	{
 	}
 }
