e5151d8a45abc0851eb3875a12c99b2b270b92eb
yahdielo <yahdielaz@gmail.com>
2022-11-02 20:49:00 +0000

    print string

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -1,24 +1,27 @@
 #include <stdarg.h>
 #include <stdio.h>
 /**
- *
+ * pint_strings - print string
+ * @separator: value to be printed between strings
+ * @n: numbers of string pass to function
  *
  */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-	int i, j;
+	unsigned int i;
+	char *j;
 	va_list p;
 
-	if (n != NULL)
+	if (n != 0)
 	{
 		/* initialising argument n as value to list p*/
 		va_start(p, n);
 
 		/*interate in string*/
-		for (i = 0; i < n: i++)
+		for (i = 0; i < n; i++)
 		{
 			/* retrive argument data*/
-			j = va_arg(p, n);
+			j = va_arg(p, char *);
 
 			/*print argiments*/
 			printf("%s", j);
@@ -27,14 +30,10 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			if (separator == NULL)
 			{}
 
-			 else if (i < n - 1)
+			if (i < n - 1)
 			{
 				printf("%s", separator);
 			}
-		}
-		else
-		{
-			printf("nil");
 		}
 
 	}
