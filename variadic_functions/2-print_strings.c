affa6c85f3cd28e6c60debc826a6d0ee3e8243c7
yahdielo <yahdielaz@gmail.com>
2022-11-03 17:50:24 +0000

    print string

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -24,6 +24,11 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			/* retrive argument data*/
 			j = va_arg(p, char *);
 
+			if (j == NULL)
+			{
+				printf("nil");
+			}
+
 			/*print argiments*/
 			printf("%s", j);
 
