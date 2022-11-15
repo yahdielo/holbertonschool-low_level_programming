8c8bf60f305f2be375847570382de8ed9ef012b6
yahdielo <yahdielaz@gmail.com>
2022-11-15 18:46:04 +0000

    free list_t

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -1,13 +1,15 @@
 #include "lists.h"
 #include <stdlib.h>
 /**
- *
+ * free_list - free struct
+ * @head: pointer to head node
+ * Return: free struct
  */
 void free_list(list_t *head)
 {
 	list_t *newnode;
 
-	while (head == NULL)
+	while (head != NULL)
 	{
 		newnode = head;
 		head = newnode->next;
