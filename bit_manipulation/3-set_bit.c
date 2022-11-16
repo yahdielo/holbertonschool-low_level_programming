af05f8408e1d7aa368066211987a018a2df6c740
yahdielo <yahdielaz@gmail.com>
2022-11-16 18:33:31 +0000

    fic betty

--- bit_manipulation/3-set_bit.c
+++ bit_manipulation/3-set_bit.c
@@ -1,7 +1,9 @@
 #include "main.h"
 /**
- *
- *
+ * set_bit - funtion to set a bit in a given index
+ * @n: is the pointer to a int inside the index
+ * @index: given index to manipulate bit
+ * Return: -1 if error and 1 if successful run
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
