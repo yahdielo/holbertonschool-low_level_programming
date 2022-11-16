52f96789b1579dc635c3eb29876b00bbd162be50
yahdielo <yahdielaz@gmail.com>
2022-11-16 17:20:01 +0000

    return binary representation of a number

--- bit_manipulation/main.h
+++ bit_manipulation/main.h
@@ -4,15 +4,4 @@
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int _putchar(char c);
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
 #endif
