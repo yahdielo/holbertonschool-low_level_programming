712521b95d8db91334a69f7d48d1f6689948f68e
yahdielo <yahdielaz@gmail.com>
2022-11-16 02:06:21 +0000

    free list

---
+++ more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * free_listint - free struct
+ * @head: pointer to head node
+ * Return: free struct
+ */
+void free_listint(listint_t *head)
+{
+	listint_t newnode;
+
+	while (head != NULL)
+	{
+		newnode = head;
+		head = newnode->next;
+		free(newnode->str);
+		free(newnode);
+	}
+}
