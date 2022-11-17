ae238de174e368ddda51999995e4c51442d19704
yahdielo <yahdielaz@gmail.com>
2022-11-17 22:39:02 +0000

    sim lists of node data

---
+++ more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+/**
+ *
+ *
+ */
+int sum(listint_t *head)
+{
+	int sum = 0;
+
+	listint_t *current_node = head;
+
+	while (head == NULL)
+	{
+		return (0);
+	}
+
+	if (current_node != NULL)
+	{
+		current_node = current_node->next;
+		sum += current_node->n;
+	}
+
+	return (sum);
+
+
+}
