49d8eb2718d0be27f5cca854409d6cf64a63bf81
yahdielo <yahdielaz@gmail.com>
2022-11-20 01:24:35 +0000

    create file

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -15,16 +15,12 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
+	if (fd == NULL)
+	{
+		return (-1);
+	}
 
-	if (text_content == NULL)
-	{
-		fd = open(filename, O_CREAT, 0600);
-	}
-	else
-	{
-	fd = open(filename, O_CREAT, 0600);
-	write(fd, text_content, buf);
-	}
+	fd = open(filename, O_CREATE | O_TRUNC, 0600);
 
 	close(fd);
 	return (1);
