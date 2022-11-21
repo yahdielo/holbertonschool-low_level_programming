e191d5487565a8fab022a1066b0682e56ec5774c
yahdielo <yahdielaz@gmail.com>
2022-11-21 01:54:10 +0000

    compy content from one file to another

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -4,7 +4,6 @@
  * @argc: argument count
  * @argv: number of arguments
  * Return: copy of content
- *
  */
 int main(int argc, char *argv[])
 {
@@ -17,30 +16,25 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-
 	fd1 = open(argv[1], O_RDONLY);
-
 	/* if file_from does not exist exit 98*/
-	if (argv[1] == NULL)
+	if (fd1 == -1 || argv[1] == NULL)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file NAME_OF_THE_FILE\n");
 		exit(98);
 	}
-
 	fd2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
-	if (argv[2] == NULL)
+	if (fd2 == -1 || argv[2] == NULL)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to NAME_OF_THE_FILE\n");
 		exit(99);
 	}
-
 	while (1)
 	{
 		read(fd1, buf, 1024);
 		write(fd2, buf, 1024);
 	}
-
 	close_fd(fd1,fd2);
 	return (0);
 }
