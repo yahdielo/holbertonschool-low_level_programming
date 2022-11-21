899b439b934c8d8e67939b89b575b3297330bbf8
yahdielo <yahdielaz@gmail.com>
2022-11-21 17:48:18 +0000

    return number of elements list

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -3,13 +3,14 @@
  * dlistint_t - return numbers of elements in list
  * Return: count of elements
  */
-size_t dlistint_t(const dlistint_t *h)
+size_t dlistint_len(const dlistint_t *h)
 {
 	int count = 0;
 
 	while (h != NULL)
 	{
 		count++;
+		h = h->next;
 	}
 	return (count);
 }
