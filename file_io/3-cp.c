390b96f9e43d3d73ccd732dbc6c0e3316f2917f0
yahdielo <yahdielaz@gmail.com>
2022-11-21 02:32:04 +0000

    compy content from one file to another

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -7,9 +7,8 @@
  */
 int main(int argc, char *argv[])
 {
-	int fd1, fd2;
+	int fd1, fd2, read_file, write_file;
 	char *buf[1024];
-
 	/* if number of arguments is not correct exit 97*/
 	if (argc != 3)
 	{
@@ -24,7 +23,6 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 	fd2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-
 	if (fd2 == -1 || argv[2] == NULL)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to NAME_OF_THE_FILE\n");
@@ -32,8 +30,19 @@ int main(int argc, char *argv[])
 	}
 	while (1)
 	{
-		read(fd1, buf, 1024);
-		write(fd2, buf, 1024);
+		read_file = read(fd1, buf, 1024);
+		if (read_file == 0) break;
+		if (read_file == -1)
+		{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			exit(98);
+		}
+		write_file = write(fd2, buf, read_file);
+		if (write_file == -1)
+		{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
+		}
 	}
 	close_fd(fd1,fd2);
 	return (0);
