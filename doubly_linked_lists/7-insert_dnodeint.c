5edd3ce1feca7ab0202a4de05eb64f7efcc0cd9d
yahdielo <yahdielaz@gmail.com>
2022-11-22 20:36:04 +0000

    inser node at index

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -12,9 +12,14 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *after = *h, *before = *h;
 	unsigned int count = 0;
 
-	if (*h == NULL)
+	if (h == NULL)
 	{
 		return (NULL);
+	}
+
+	if (idx == 0)
+	{
+		add_dnodeint(h, n);
 	}
 
 	if (newnode == NULL)
