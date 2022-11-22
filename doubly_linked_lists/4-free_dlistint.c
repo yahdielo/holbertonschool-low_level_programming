c7cbaa67e64344aa35885cc4c33b0e5619fbab61
yahdielo <yahdielaz@gmail.com>
2022-11-22 15:28:57 +0000

    free node in list

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_dlistint - free node list
+ * *head: pointer to stuct
+ * Return: free new node
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *newnode;
+
+	while (head != NULL)
+	{
+		newnode = head;
+		head = newnode->next;
+		free(newnode);
+	}
+}
