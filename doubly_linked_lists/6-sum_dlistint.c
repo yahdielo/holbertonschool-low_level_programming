5e748162903f242cd0bfae9dc1e4447a2dfb9f2d
yahdielo <yahdielaz@gmail.com>
2022-11-22 17:46:08 +0000

    sum al n elements of node list

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * sum_dlisting - sum all the elemetns inside list of nodes
+ * @head - pointer to head node
+ * Return: sum
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;	
+	}
+	return (sum);
+}
