1b0847e00f4e7e34eeca17c56a428264fcab2b20
yahdielo <yahdielaz@gmail.com>
2022-11-23 20:38:50 +0000

    add a new element to hash table

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -12,12 +12,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	hash_node_t *node = malloc(sizeof(hash_node_t));
 
-	if (ht == NULL)
-	{
-		return (0);
-	}
-
-	if (key == NULL)
+	if (ht == NULL || key == 0)
 	{
 		return (0);
 	}
