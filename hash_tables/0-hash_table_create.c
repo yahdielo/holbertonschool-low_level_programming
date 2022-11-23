d0b1e6f8d15f4be8d1714990d67bccd5730c1f59
yahdielo <yahdielaz@gmail.com>
2022-11-23 17:43:22 +0000

    creat a hash table

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -10,14 +10,14 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	hash_table_t *table = malloc(sizeof(hash_table_t));
 
-	table->size = size;
-
-	table->array = malloc(sizeof(hash_node_t *) * table->size);
-
-	if(table == NULL)
+	if (table == NULL)
 	{
 		return (NULL);
 	}
+
+	table->size = size;
+
+	table->array = malloc(sizeof(hash_node_t *) * table->size);
 
 	for (i = 0; i < table->size; i++)
 	{
