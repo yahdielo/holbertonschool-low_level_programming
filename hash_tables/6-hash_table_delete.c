63f31c0a8652f778d1f42dffadb60d6664ef3f47
yahdielo <yahdielaz@gmail.com>
2022-11-27 01:50:31 +0000

    delet hash table

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -9,23 +9,25 @@ void hash_table_delete(hash_table_t *ht)
 
 	unsigned long int i = 0;
 
-	if (ht->array[i] == NULL)
-	{
-		free(ht->array[i]);
-	}
-
-	node = ht->array[i];
-	delete_node = ht->array[i];
-
 	for (i = 0; i < ht->size; i++)
 	{
-		while (node != NULL)
+		if (ht->array[i] == NULL)
 		{
-			delete_node = node;
-			free(delete_node->key);
-			free(delete_node->value);
-			free(delete_node);
-			node = node->next;
+			free(ht->array[i]);
+		}
+		else
+		{
+			node = ht->array[i];
+			delete_node = ht->array[i];
+
+			while (node != NULL)
+			{
+				delete_node = node;
+				free(delete_node->key);
+				free(delete_node->value);
+				free(delete_node);
+				node = node->next;
+			}
 		}
 	}
 	free(ht->array);
