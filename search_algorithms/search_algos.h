7bf92bdbfd25bed57041e54f4b5e7f5574df4d47
yahdielo <yahdielaz@gmail.com>
2023-01-03 17:20:54 +0000

    fix .h file

--- search_algorithms/search_algos.h
+++ search_algorithms/search_algos.h
@@ -1,6 +1,7 @@
-#ifndef SEARCH_ALGORTHMS_H
-#define SEARCH_ALGORITHMDS_H
+#ifndef SEARCH_ALGORITHMS_H
+#define SEARCH_ALGORITHMS_H
 #include <stdio.h>
 #include <stdlib.h>
+
 int linear_search(int *array, size_t size, int value);
 #endif
